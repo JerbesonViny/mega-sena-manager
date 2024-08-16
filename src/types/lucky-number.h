582bea04f696947a1734b2553d60d46650da9df5
Jerbeson Vinycius <jerbesonvinycius@gmail.com>
2024-08-15 22:03:18 -0300

    🏷️ BestLuckyNumber and SortOrder

---
+++ src/types/lucky-number.h
@@ -0,0 +1,10 @@
+#ifndef LUCKY_NUMBER_TYPE_H
+#define LUCKY_NUMBER_TYPE_H
+
+typedef struct BestLuckyNumber
+{
+    int lucky_number;
+    int incidence;
+} BestLuckyNumber;
+
+#endif
---
+++ src/types/sort.h
@@ -0,0 +1,10 @@
+#ifndef SORT_TYPES_H
+#define SORT_TYPES_H
+
+typedef enum SortOrder
+{
+    ASCENDING,
+    DESCENDING
+} SortOrder;
+
+#endif
