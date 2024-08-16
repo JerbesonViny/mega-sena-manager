92002ddf661b85001b1a0c73408a1008872083d6
Jerbeson Vinycius <jerbesonvinycius@gmail.com>
2024-08-15 22:04:09 -0300

    ✨ Adding integer helpers

---
+++ src/helpers/integers/integers.c
@@ -0,0 +1,11 @@
+#include "integers.h"
+
+int left_values_is_greater_than_right_value(int left_value, int right_value)
+{
+    return left_value > right_value;
+}
+
+int left_values_is_less_than_right_value(int left_value, int right_value)
+{
+    return left_value < right_value;
+}
---
+++ src/helpers/integers/integers.h
@@ -0,0 +1,7 @@
+#ifndef INTEGERS_HELPER_H
+#define INTEGERS_HELPER_H
+
+int left_values_is_greater_than_right_value(int left_value, int right_value);
+int left_values_is_less_than_right_value(int left_value, int right_value);
+
+#endif
